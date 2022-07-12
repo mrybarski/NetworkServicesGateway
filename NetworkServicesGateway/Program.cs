430f4d6a7da4f7ef8c3f0b41caeda2440cdb61b1
Marcin Rybarski <marcin_rybarski@polcom.com.pl>
2022-07-12 21:48:16 +0200

    Dodanie PingService

--- NetworkServicesGateway/Program.cs
+++ NetworkServicesGateway/Program.cs
@@ -1,8 +1,11 @@
+using NetworkServicesGateway.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+var asd = new PingService();
+asd.RunService("8.8.8.8");
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ NetworkServicesGateway/Services/PingService.cs
@@ -0,0 +1,53 @@
+﻿using System.Net;
+using System.Net.NetworkInformation;
+namespace NetworkServicesGateway.Services
+{
+    public class PingService : IDisposable
+    {
+        private readonly Ping ping;
+        private CancellationTokenSource cancellationTokenSource;
+        private TimeSpan sendPingInterval;
+
+        public PingService()
+        {
+            ping = new Ping();
+            cancellationTokenSource = new();
+            sendPingInterval = TimeSpan.FromSeconds(1);
+        }
+
+        public void RunService(string ipAdressStr)
+        {
+            if (!IPAddress.TryParse(ipAdressStr, out var ipAddress)) 
+                throw new ArgumentException("Błędny adres IP", nameof(ipAdressStr));
+
+            SendPing(ipAddress, cancellationTokenSource.Token);
+        }
+
+        public void Dispose()
+        {
+            cancellationTokenSource?.Cancel();
+            cancellationTokenSource?.Dispose();
+
+            ping.SendAsyncCancel();
+        }
+
+        private void SendPing(IPAddress ipAdress, CancellationToken cancellationToken)
+        {
+            try
+            {
+                if (cancellationToken.IsCancellationRequested)
+                    return;
+
+                var reply = ping.Send(ipAdress);
+                System.Diagnostics.Debug.WriteLine(reply.RoundtripTime);
+
+                Task.Delay(sendPingInterval, cancellationToken)
+                        .GetAwaiter()
+                        .OnCompleted(() => SendPing(ipAdress, cancellationToken));
+            }
+            catch
+            {
+            }
+        }
+    }
+}
